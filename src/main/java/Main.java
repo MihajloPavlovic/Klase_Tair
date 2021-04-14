7f7a8f1395b0114a73cd48f907db4139fb751179
Mihajlo <mihajlo0709@gmail.com>
2021-04-14 13:45:25 +0200

    prosecna_potrosnja_goriva

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+       
+        Automobil a = new Automobil();
+        
+        a.dopuniGorivo(35.0);
+        
+        a.voziPoGradu(3.0);
+        a.voziNaOtvorenom(80.0);
+        
+        a.voziPoGradu(5.6);
+        a.voziPoGradu(1.2);
+        a.voziPoGradu(100.2);
+        
+        a.dopuniGorivo(5.0);
+        
+        a.voziPoGradu(1.5);
+        a.voziNaOtvorenom(1.5);
+        
+        a.dopuniGorivo(5.0);
+        
+        
+    }
+    
+}
